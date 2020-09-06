4d4c89e44f3ba301fe42d8054e6fd92389dc29bb
dikac <rigel234@yahoo.com>
2020-09-06 08:11:24 +0700

    commit dist

--- dist/request/middleware/property-validator.d.ts
+++ dist/request/middleware/property-validator.d.ts
@@ -3,5 +3,4 @@ import ValidatorInterface from "@dikac/t-validator/simple";
 import { Middleware } from "@koa/router";
 import Instance from "@dikac/t-validator/validatable/validatable";
 import { Request } from "koa";
-import Body from "@dikac/t-http/body/body";
-export default function PropertyValidator<BodyType = unknown, RequestType extends Request = Request>(validator: ValidatorInterface<Body, Body, Instance<Body>>, key: keyof RequestType, failCode?: number): Middleware;
+export default function PropertyValidator<BodyType = unknown, RequestType extends Request = Request>(validator: ValidatorInterface<BodyType, BodyType, Instance<BodyType>>, key: keyof RequestType, failCode?: number): Middleware;
