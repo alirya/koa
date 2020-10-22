41f79b75ae026f1137cdd8571148dfdecad3362c
dikac <rigel234@yahoo.com>
2020-10-22 18:36:46 +0700

    improve route/route type

--- dist/route/route.d.ts
+++ dist/route/route.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="koa__router" />
-import * as Router from "@koa/router";
+import Router from "@koa/router";
 import Path from "@dikac/t-http/request/path/path";
 import Method from "@dikac/t-http/request/method/method";
 import { Middleware } from "@koa/router";
-export default class Route {
-    router: Router;
+export default class Route<StateType = any, CustomType = {}> {
+    router: Router<StateType, CustomType>;
     route: Method & Path;
-    constructor(router: Router, route: Method & Path);
-    use(middleware: Middleware): void;
+    constructor(router: Router<StateType, CustomType>, route: Method & Path);
+    use(middleware: Middleware<StateType, CustomType>): void;
 }
--- src/route/route.ts
+++ src/route/route.ts
@@ -1,17 +1,17 @@
-import * as Router from "@koa/router";
+import Router from "@koa/router";
 import Path from "@dikac/t-http/request/path/path";
 import Method from "@dikac/t-http/request/method/method";
 import {Middleware} from "@koa/router";
 
-export default class Route {
+export default class Route<StateType = any, CustomType = {}> {
 
     constructor(
-        public router : Router,
+        public router : Router<StateType, CustomType>,
         public route : Method & Path
     ) {
     }
 
-    use(middleware : Middleware) {
+    use(middleware : Middleware<StateType, CustomType>) {
 
         this.router.register(this.route.path, [this.route.method], middleware);
     }
