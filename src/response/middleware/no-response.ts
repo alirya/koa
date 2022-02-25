f1520670ad7615ff5bb6b346ca1bc17d727d6eb7
dikac <rigel234@yahoo.com>
2022-02-25 12:57:19 +0700

    re add no-response

---
+++ src/response/middleware/no-response.ts
@@ -0,0 +1,25 @@
+import Context from "../../context/context";
+import {Middleware} from "koa";
+
+/**
+ * @deprecated
+ *
+ * @param middleware
+ * @constructor
+ */
+export default function NoResponse(
+    middleware : Middleware
+) : Middleware {
+
+    return function (context : Context, next) {
+
+        if(context.response.status === 404 && context.response.body === undefined) {
+
+            return middleware(context, next);
+
+        } else {
+
+            return next();
+        }
+    }
+}
