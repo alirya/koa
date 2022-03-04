c8207c58d60f2a4cd80d3eb1491ce85bc1ccd16a
dikac <rigel234@yahoo.com>
2022-03-05 01:37:17 +0700

    fix name

--- src/syslog/handler/print-request.ts
+++ src/syslog/handler/print-request.ts
@@ -2,7 +2,7 @@ import Log from "@alirya/syslog/syslog";
 import Handler from "./handler";
 import Koa from "koa";
 
-export default function PrintResponse<ContextType extends Koa.DefaultContext>(
+export default function PrintRequest<ContextType extends Koa.DefaultContext>(
     severity : keyof Log = 'debug',
 ) : Handler<Log<[string, any, any]>, ContextType> {
 
